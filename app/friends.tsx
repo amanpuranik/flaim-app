625abe3d37e822f60ec6e9a6b8602f843a0c89ad
Aaliyan <aaliyan@epochapp.com>
2024-02-04 00:34:55 -0500

    Services for each page

--- app/friends.tsx
+++ app/friends.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import { Button, Text } from 'react-native-paper';
-import Wrapper from './components/Wrapper';
+import React, { useState, useEffect } from "react";
+import { Button, Text } from "react-native-paper";
+import Wrapper from "./components/Wrapper";
 import { View } from "react-native";
-import { router } from 'expo-router';
+import { router } from "expo-router";
+import { getFriends } from "./services/friendService";
 
-export default function test() {
+export default function friends() {
+  const [friendsList, setFriendsList] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchFriends = async () => {
+      try {
+        const friends = await getFriends();
+        setFriendsList(friends);
+      } catch (error) {
+        console.error("Failed to fetch friends: ", error);
+      }
+    };
+
+    fetchFriends();
+  }, []);
+
   return (
-    <Wrapper title="Friends" leftIcon='arrow-left' leftIconAction={() => router.back()}>
+    <Wrapper
+      title="Friends"
+      leftIcon="arrow-left"
+      leftIconAction={() => router.back()}
+    >
       <View className="justify-center items-center h-full w-full">
         <Text>FRIENDS</Text>
       </View>
-    </Wrapper >
+    </Wrapper>
   );
 }
-
-
--- app/helpers/auth.ts
+++ app/helpers/auth.ts
@@ -1,8 +1,8 @@
 import {
-    User,
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signInWithEmailAndPassword,
+  User,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
 } from "firebase/auth";
 import { auth } from "../../firebase";
 import { AuthResponse } from "../constants/types";
@@ -11,88 +11,87 @@ import * as store from "../helpers/store";
 import { router } from "expo-router";
 
 export async function signup(
-    email: string,
-    password: string
+  email: string,
+  password: string
 ): Promise<AuthResponse> {
-    try {
-        const t = await createUserWithEmailAndPassword(auth, email, password);
-        return {
-            token: t,
-            error: false,
-            errorMessage: "",
-        };
-    } catch (error: any) {
-        let readableErrorMessage = "";
-        switch (error.code) {
-            case "auth/email-already-in-use":
-                readableErrorMessage = "This email is already in use.";
-                break;
-            case "auth/invalid-email":
-                readableErrorMessage = "Invalid email address.";
-                break;
-            case "auth/operation-not-allowed":
-                readableErrorMessage = "Email/password accounts are not enabled.";
-                break;
-            case "auth/weak-password":
-                readableErrorMessage = "Please choose a stronger password";
-                break;
-            default:
-                readableErrorMessage = "An unexpected error occurred.";
-                break;
-        }
-        return {
-            token: null,
-            error: true,
-            errorMessage: readableErrorMessage,
-        };
+  try {
+    const t = await createUserWithEmailAndPassword(auth, email, password);
+    return {
+      token: t,
+      error: false,
+      errorMessage: "",
+    };
+  } catch (error: any) {
+    let readableErrorMessage = "";
+    switch (error.code) {
+      case "auth/email-already-in-use":
+        readableErrorMessage = "This email is already in use.";
+        break;
+      case "auth/invalid-email":
+        readableErrorMessage = "Invalid email address.";
+        break;
+      case "auth/operation-not-allowed":
+        readableErrorMessage = "Email/password accounts are not enabled.";
+        break;
+      case "auth/weak-password":
+        readableErrorMessage = "Please choose a stronger password";
+        break;
+      default:
+        readableErrorMessage = "An unexpected error occurred.";
+        break;
     }
+    return {
+      token: null,
+      error: true,
+      errorMessage: readableErrorMessage,
+    };
+  }
 }
 
 export async function signin(
-    email: string,
-    password: string
+  email: string,
+  password: string
 ): Promise<AuthResponse> {
-    try {
-        const t = await signInWithEmailAndPassword(auth, email, password);
-        return {
-            token: t,
-            error: false,
-            errorMessage: "",
-        };
-    } catch (error: any) {
-        let readableErrorMessage = "";
-        switch (error.code) {
-            case "auth/user-not-found":
-                readableErrorMessage = "No user found with this email.";
-                break;
-            case "auth/wrong-password":
-                readableErrorMessage = "Incorrect password.";
-                break;
-            case "auth/user-disabled":
-                readableErrorMessage = "This user has been disabled.";
-                break;
-            case "auth/too-many-requests":
-                readableErrorMessage = "Too many requests. Try again later.";
-                break;
-            case "auth/invalid-email":
-                readableErrorMessage = "Invalid email address.";
-                break;
-            default:
-                readableErrorMessage = "An unexpected error occurred.";
-                break;
-        }
-        return {
-            token: null,
-            error: true,
-            errorMessage: readableErrorMessage,
-        };
+  try {
+    const t = await signInWithEmailAndPassword(auth, email, password);
+    return {
+      token: t,
+      error: false,
+      errorMessage: "",
+    };
+  } catch (error: any) {
+    let readableErrorMessage = "";
+    switch (error.code) {
+      case "auth/user-not-found":
+        readableErrorMessage = "No user found with this email.";
+        break;
+      case "auth/wrong-password":
+        readableErrorMessage = "Incorrect password.";
+        break;
+      case "auth/user-disabled":
+        readableErrorMessage = "This user has been disabled.";
+        break;
+      case "auth/too-many-requests":
+        readableErrorMessage = "Too many requests. Try again later.";
+        break;
+      case "auth/invalid-email":
+        readableErrorMessage = "Invalid email address.";
+        break;
+      default:
+        readableErrorMessage = "An unexpected error occurred.";
+        break;
     }
+    return {
+      token: null,
+      error: true,
+      errorMessage: readableErrorMessage,
+    };
+  }
 }
 
 export function signOut() {
-    auth.signOut();
-    router.replace("/login")
-
+  auth.signOut();
+  router.replace("/login");
 }
 
 // export function getUser() {
