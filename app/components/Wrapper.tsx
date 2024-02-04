e953abbea4f15ee3c965f5ac5869a5d05c4ce65b
Aaliyan <aaliyan@epochapp.com>
2024-02-03 21:38:51 -0500

    Sign in and Register Error validation

--- app/components/Wrapper.tsx
+++ app/components/Wrapper.tsx
@@ -9,108 +9,116 @@ import { auth } from "../../firebase";
 import { signOut } from "../helpers/auth";
 
 type HeaderProps = {
-    title?: String;
-    leftIcon?: "back" | "close" | "friends";
-    rightIcon?: "profile" | "stats";
-    closeFunction?: () => void;
-    openStats?: () => void;
-    children: any;
-}
+  title?: String;
+  leftIcon?: "back" | "close" | "friends";
+  rightIcon?: "profile" | "stats";
+  closeFunction?: () => void;
+  openStats?: () => void;
+  children: any;
+};
 
 function getIcon(iconName: String): IconSource {
-    switch (iconName) {
-        case "close":
-            return "close"
-        case "back":
-            return "arrow-left"
-        case "profile":
-            return "account"
-        case "friends":
-            return "account-group"
-        case "stats":
-            return "crown"
-        default:
-            return ""
-    }
+  switch (iconName) {
+    case "close":
+      return "close";
+    case "back":
+      return "arrow-left";
+    case "profile":
+      return "account";
+    case "friends":
+      return "account-group";
+    case "stats":
+      return "crown";
+    default:
+      return "";
+  }
 }
 
 export default function Wrapper(props: HeaderProps) {
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (!user) {
-                signOut();
-                router.replace("/login")
-            }
-        })
-    }, [])
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        signOut();
+        router.replace("/login");
+      }
+    });
+  }, []);
 
-    let clr = useTheme().colors;
+  let clr = useTheme().colors;
 
-    const leftIcon = props.leftIcon;
-    const rightIcon = props.rightIcon;
+  const leftIcon = props.leftIcon;
+  const rightIcon = props.rightIcon;
 
-    const iconWidth = "w-1/5";
+  const iconWidth = "w-1/5";
 
-    return (
-        <SafeAreaView style={{ backgroundColor: clr.background }} className={`flex-1 px-5 pt-4`}>
-            <View className="flex-row items-center justify-between">
-                {leftIcon == "back" &&
-                    <TouchableOpacity onPress={() => router.back()} className={iconWidth}>
-                        <IconButton
-                            icon={getIcon(leftIcon)}
-                            iconColor={clr.primary}
-                            size={32}
-                        />
-                    </TouchableOpacity>
-                }
-                {leftIcon == "close" &&
-                    <TouchableOpacity onPress={() => props.closeFunction} className={iconWidth}>
-                        <IconButton
-                            icon={getIcon(leftIcon)}
-                            iconColor={clr.primary}
-                            size={32}
-                        />
-                    </TouchableOpacity>
-                }
-                {leftIcon == "friends" &&
-                    <TouchableOpacity onPress={() => router.push("/friends")} className={iconWidth}>
-                        <IconButton
-                            icon={getIcon(leftIcon)}
-                            iconColor={clr.primary}
-                            size={32}
-                        />
-                    </TouchableOpacity>
-                }
-                {!leftIcon &&
-                    <View className={iconWidth}></View>
-                }
-                {props.title &&
-                    <Text variant={'headlineSmall'}>{props.title}</Text>
-
-                }
-                {rightIcon == "profile" &&
-                    <TouchableOpacity onPress={() => router.push("/profile")} className={iconWidth}>
-                        <IconButton
-                            icon={getIcon(rightIcon)}
-                            iconColor={clr.primary}
-                            size={32}
-                        />
-                    </TouchableOpacity>
-                }
-                {rightIcon == "stats" &&
-                    <TouchableOpacity onPress={() => props.openStats} className={iconWidth}>
-                        <IconButton
-                            icon={getIcon(rightIcon)}
-                            iconColor={clr.primary}
-                            size={32}
-                        />
-                    </TouchableOpacity>
-                }
-                {!rightIcon &&
-                    <View className={iconWidth}></View>
-                }
-            </View>
-            {props.children}
-        </SafeAreaView>
-    )
+  return (
+    <SafeAreaView
+      style={{ backgroundColor: clr.background }}
+      className={`flex-1 px-5 pt-4`}
+    >
+      <View className="flex-row items-center justify-between">
+        {leftIcon == "back" && (
+          <TouchableOpacity onPress={() => router.back()} className={iconWidth}>
+            <IconButton
+              icon={getIcon(leftIcon)}
+              iconColor={clr.primary}
+              size={32}
+            />
+          </TouchableOpacity>
+        )}
+        {leftIcon == "close" && (
+          <TouchableOpacity
+            onPress={() => props.closeFunction}
+            className={iconWidth}
+          >
+            <IconButton
+              icon={getIcon(leftIcon)}
+              iconColor={clr.primary}
+              size={32}
+            />
+          </TouchableOpacity>
+        )}
+        {leftIcon == "friends" && (
+          <TouchableOpacity
+            onPress={() => router.push("/friends")}
+            className={iconWidth}
+          >
+            <IconButton
+              icon={getIcon(leftIcon)}
+              iconColor={clr.primary}
+              size={32}
+            />
+          </TouchableOpacity>
+        )}
+        {!leftIcon && <View className={iconWidth}></View>}
+        {props.title && <Text variant={"headlineSmall"}>{props.title}</Text>}
+        {rightIcon == "profile" && (
+          <TouchableOpacity
+            onPress={() => router.push("/profile")}
+            className={iconWidth}
+          >
+            <IconButton
+              icon={getIcon(rightIcon)}
+              iconColor={clr.primary}
+              size={32}
+            />
+          </TouchableOpacity>
+        )}
+        {rightIcon == "stats" && (
+          <TouchableOpacity
+            onPress={() => props.openStats}
+            className={iconWidth}
+          >
+            <IconButton
+              icon={getIcon(rightIcon)}
+              iconColor={clr.primary}
+              size={32}
+            />
+          </TouchableOpacity>
+        )}
+        {!rightIcon && <View className={iconWidth}></View>}
+      </View>
+      {props.children}
+    </SafeAreaView>
+  );
 }
--- app/login.tsx
+++ app/login.tsx
@@ -1,93 +1,113 @@
-import { View, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import { Button, TextInput, useTheme, Text, IconButton } from 'react-native-paper';
-import { router } from 'expo-router';
-import { signin } from './helpers/auth';
-import { AuthResponse } from './constants/types';
+import { View, KeyboardAvoidingView, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  TextInput,
+  useTheme,
+  Text,
+  IconButton,
+} from "react-native-paper";
+import { router } from "expo-router";
+import { signin } from "./helpers/auth";
+import { AuthResponse } from "./constants/types";
 
 export default function Login() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const [loginBtnText, setLoginBtnText] = useState("Login")
-    const [loginError, setLoginError] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [loginBtnText, setLoginBtnText] = useState("Login");
+  const [loginError, setLoginError] = useState<String>("");
 
-    let clr = useTheme().colors;
+  let clr = useTheme().colors;
 
-    useEffect(() => {
-        loading ? setLoginBtnText("Logging in...") : setLoginBtnText("Login")
-    }, [loading])
+  useEffect(() => {
+    loading ? setLoginBtnText("Logging in...") : setLoginBtnText("Login");
+  }, [loading]);
 
-    async function login() {
-        if (email && password) {
-            setLoading(true);
-            const token: AuthResponse = await signin(email, password);
-            if (token.error) {
-                console.log(token)
-                setLoginError(true)
-            } else {
-                router.replace("/feed")
-            }
-            setLoading(false)
-        }
+  async function login() {
+    if (email && password) {
+      setLoading(true);
+      const response: AuthResponse = await signin(email, password);
+      if (response.error) {
+        console.log(response.errorMessage);
+        setLoginError(response.errorMessage);
+      } else {
+        router.replace("/feed");
+      }
+      setLoading(false);
+    } else {
+      setLoginError("Please enter both email and password.");
     }
+  }
 
-    return (
-        <KeyboardAvoidingView style={{ backgroundColor: clr.background }} className={`justify-center items-center h-full w-full`}>
-            <View className='flex-row items-center'>
-                <Text className='font-semibold' variant='displayMedium'>
-                    Login
-                </Text>
-                <IconButton
-                    icon="login"
-                    iconColor={clr.primary}
-                    size={48}
-                />
-            </View>
-            <TextInput
-                style={{ backgroundColor: clr.surfaceVariant }}
-                className="w-5/6 mt-10 h-14"
-                placeholder='Email...'
-                returnKeyType="done"
-                value={email}
-                onChangeText={text => setEmail(text)}
-                error={false}
-                autoCapitalize="none"
-                autoComplete="email"
-                textContentType="emailAddress"
-                keyboardType="email-address"
-                mode='outlined'
-                outlineColor={clr.primary}
-                outlineStyle={{ borderRadius: 10, borderWidth: 0 }}
-
-            />
-            <TextInput
-                style={{ backgroundColor: clr.surfaceVariant }}
-                className="w-5/6 mt-3 h-14"
-                placeholder="Password..."
-                returnKeyType="done"
-                value={password}
-                onChangeText={text => setPassword(text)}
-                error={false}
-                secureTextEntry
-                mode='outlined'
-                outlineColor={clr.primary}
-                outlineStyle={{ borderRadius: 10, borderWidth: 0 }}
-            />
-            {loginError && <Text style={{ color: clr.error }} className='w-2/3 text-center text-xs mt-1'>
-                There was a problem logging you in. Make sure the email and password you entered are correct.
-            </Text>}
-            <Button loading={loading} contentStyle={{ height: 50 }} className='w-3/4 mt-5' mode='contained' onPress={() => login()}>{loginBtnText}</Button>
-            <View className='flex-row mt-5'>
-                <Text className='font-bold'>
-                    Don't have an account?{" "}
-                </Text>
-                <TouchableOpacity onPress={() => router.replace("/register")} className='items-center'>
-                    <Text style={{ color: clr.primary }} className="font-bold">
-                        Register
-                    </Text>
-                </TouchableOpacity>
-            </View>
-        </KeyboardAvoidingView>
-    )
+  return (
+    <KeyboardAvoidingView
+      style={{ backgroundColor: clr.background }}
+      className={`justify-center items-center h-full w-full`}
+    >
+      <View className="flex-row items-center">
+        <Text className="font-semibold" variant="displayMedium">
+          Login
+        </Text>
+        <IconButton icon="login" iconColor={clr.primary} size={48} />
+      </View>
+      <TextInput
+        style={{ backgroundColor: clr.surfaceVariant }}
+        className="w-5/6 mt-10 h-14"
+        placeholder="Email"
+        returnKeyType="done"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        error={false}
+        autoCapitalize="none"
+        autoComplete="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+        mode="outlined"
+        outlineColor={clr.primary}
+        outlineStyle={{ borderRadius: 10, borderWidth: 0 }}
+      />
+      <TextInput
+        style={{ backgroundColor: clr.surfaceVariant }}
+        className="w-5/6 mt-3 h-14"
+        placeholder="Password"
+        returnKeyType="done"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        error={false}
+        secureTextEntry
+        mode="outlined"
+        outlineColor={clr.primary}
+        outlineStyle={{ borderRadius: 10, borderWidth: 0 }}
+      />
+      {loginError && (
+        <Text
+          style={{ color: clr.error }}
+          className="w-2/3 text-center text-xs mt-1"
+        >
+          {loginError}
+        </Text>
+      )}
+      <Button
+        loading={loading}
+        contentStyle={{ height: 50 }}
+        className="w-3/4 mt-5"
+        mode="contained"
+        onPress={() => login()}
+      >
+        {loginBtnText}
+      </Button>
+      <View className="flex-row mt-5">
+        <Text className="font-bold">Don't have an account? </Text>
+        <TouchableOpacity
+          onPress={() => router.replace("/register")}
+          className="items-center"
+        >
+          <Text style={{ color: clr.primary }} className="font-bold">
+            Register
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
