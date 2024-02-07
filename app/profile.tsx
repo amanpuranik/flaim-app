106636bf4b275b77b72096086a5b4db9eee76de7
amanpuranik <56619430+amanpuranik@users.noreply.github.com>
2024-02-06 21:19:14 -0500

    add toast notif functionality

--- app/profile.tsx
+++ app/profile.tsx
@@ -3,9 +3,21 @@ import { Button, Text } from 'react-native-paper';
 import Wrapper from './components/Wrapper';
 import { Avatar } from 'react-native-paper';
 import { router } from 'expo-router';
-import { TouchableOpacity, View } from 'react-native';
+import { TouchableOpacity, View, Clipboard, ToastAndroid } from 'react-native';
+import { toast, ToastPosition, Toasts } from '@backpackapp-io/react-native-toast';
 
 export default function profile() {
+
+  const handleCopyText = async () => {
+    const textToCopy = "Link to profile";
+    await Clipboard.setString(textToCopy);
+    console.log('text copied');
+    toast.success("Profile link copied", {
+      width: 300,
+      position: ToastPosition.BOTTOM
+    });
+  };
+
   // Render initials for now, we'll deal with image logic later
   return (
     <Wrapper
@@ -16,29 +28,34 @@ export default function profile() {
       rightIconAction={() => router.push("/settings")}
 
     >
+      <Toasts />
       <View className="items-center h-full w-full">
         <Avatar.Text size={84} label="AP" />
         <Text className="pt-4">bio will go her </Text>
-        <Text className="pt-1">Link to profile</Text>
-        <View className="flex flex-row">
+        <View style={{ flexDirection: 'row', display: 'flex' }}>
+          <Button icon="content-copy" onPress={handleCopyText}>
+            <Text style={{ paddingTop: 4 }}>Link to profile</Text>
+          </Button>
+        </View>
+        <View style={{ flexDirection: 'row' }}>
           <Text className="pt-10 pr-72 self-start ">Pinned posts</Text>
         </View>
         <View style={{ flexDirection: 'row', gap: 10 }}>
-          <TouchableOpacity onPress={() => router.push("/goal")} className='mt-10 border-solid border-gray-300 rounded-3xl border-4 h-1/2 w-full bg-gray-300 h-32 w-32'>
+          <TouchableOpacity onPress={() => router.push("/goal")} className='mt-4 border-solid border-gray-300 rounded-3xl border-4 h-1/2 w-full bg-gray-300 h-32 w-32'>
             <TouchableOpacity onPress={() => router.push("/goal-feed")} className='mt-2 ml-5'>
               <Text className="font-bold text-lg text-black">
                 {"<GOAL_NAME>"}
               </Text>
             </TouchableOpacity>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.push("/goal")} className='mt-10 border-solid border-gray-300 rounded-3xl border-4 h-1/2 w-full bg-gray-300 h-32 w-32'>
+          <TouchableOpacity onPress={() => router.push("/goal")} className='mt-4 border-solid border-gray-300 rounded-3xl border-4 h-1/2 w-full bg-gray-300 h-32 w-32'>
             <TouchableOpacity onPress={() => router.push("/goal-feed")} className='mt-2 ml-5'>
               <Text className="font-bold text-lg text-black">
                 {"<GOAL_NAME>"}
               </Text>
             </TouchableOpacity>
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => router.push("/goal")} className='mt-10 border-solid border-gray-300 rounded-3xl border-4 h-1/2 w-full bg-gray-300 h-32 w-32'>
+          <TouchableOpacity onPress={() => router.push("/goal")} className='mt-4 border-solid border-gray-300 rounded-3xl border-4 h-1/2 w-full bg-gray-300 h-32 w-32'>
             <TouchableOpacity onPress={() => router.push("/goal-feed")} className='mt-2 ml-5'>
               <Text className="font-bold text-lg text-black">
                 {"<GOAL_NAME>"}
--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-paper/babel', 'expo-router/babel', 'nativewind/babel'],
+    plugins: ['react-native-paper/babel', 'expo-router/babel', 'nativewind/babel', "react-native-reanimated/plugin"],
+
   };
 };
